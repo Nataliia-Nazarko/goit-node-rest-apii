3a564e5224020c8270d0b4102747ecbe6f7d41ae
Nataliia Nazar <you@example.com>
2024-05-20 17:36:02 +0200

    Tasks are done

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,65 @@
+import * as contactsService from "../services/contactsServices.js";
+import ctrlWrapper from "../decorators/ctrlWrapper.js";
+import HttpError from "../helpers/HttpError.js";
+
+const getAllContacts = async (req, res) => {
+  const result = await contactsService.listContacts();
+
+  res.status(200).json(result);
+};
+
+const getOneContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.getContactById(id);
+
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+
+  res.status(200).json(result);
+};
+
+const createContact = async (req, res) => {
+  const result = await contactsService.addContact(req.body);
+
+  res.status(201).json(result);
+};
+
+const updateContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.updateContactById(id, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.json(result);
+};
+
+const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.removeContact(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.status(200).json(result);
+};
+
+const updateFavoriteStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contactsService.updateStatusContact(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.status(200).json(result);
+};
+
+export default {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getOneContact: ctrlWrapper(getOneContact),
+  createContact: ctrlWrapper(createContact),
+  updateContact: ctrlWrapper(updateContact),
+  deleteContact: ctrlWrapper(deleteContact),
+  updateFavoriteStatusContact: ctrlWrapper(updateFavoriteStatusContact),
+};
