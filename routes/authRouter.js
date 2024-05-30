833814fd125f861b643c0fbc3548de5743be7ead
Nataliia Nazar <you@example.com>
2024-05-30 22:20:02 +0200

    router

---
+++ routes/authRouter.js
@@ -0,0 +1,53 @@
+import express from "express";
+
+import upload from "../middlewares/upload.js";
+import isEmptyBody from "../middlewares/isEmptyBody.js";
+
+import validateBody from "../decorators/validateBody.js";
+
+import {
+  usersRegister,
+  usersLogin,
+  updateUserSubscriptionSchema,
+} from "../schemas/authSchema.js";
+
+import authController from "../controllers/authControllers.js";
+
+import authenticate from "../middlewares/authenticate.js";
+
+const authRouter = express.Router();
+
+authRouter.post(
+  "/register",
+  isEmptyBody,
+  validateBody(usersRegister),
+  authController.register
+);
+
+authRouter.post(
+  "/login",
+  isEmptyBody,
+  validateBody(usersLogin),
+  authController.login
+);
+
+authRouter.get("/current", authenticate, authController.getCurrent);
+
+authRouter.post("/logout", authenticate, authController.logout);
+
+authRouter.patch(
+  "/",
+  authenticate,
+  isEmptyBody,
+  validateBody(updateUserSubscriptionSchema),
+  authController.updateSubscription
+);
+
+authRouter.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  authController.updateAvatar
+);
+
+export default authRouter;
